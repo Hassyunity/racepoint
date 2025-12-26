be2d566b3e460da60f224eff03e5c054fe7e19cc
Hassyunity <hassy.tsihoarana@gmail.com>
2025-12-26 15:40:35 +0300

    allow host render

--- racepont-frontend/vite.config.ts
+++ racepont-frontend/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: '0.0.0.0',                         // écoute toutes les interfaces réseau
+    allowedHosts: ['racepoint-h.onrender.com'], // autorise le host Render
+  },
 })
